b1595b7952fccd61d6ea8fd179ca01e5c85c0dff
Charles Monteiro <charles@nycsmalltalk.org>
2011-12-16 12:36:37 -0500

    second test commit , added another rubymine project

---
+++ TestGIT/MyClass2.rb
@@ -0,0 +1,18 @@
+class MyClass2
+  # To change this template use File | Settings | File Templates.
+  #Pre-requisites
+
+
+  #Instance Variables
+
+
+  #Class Variables
+
+
+  #Methods
+
+  def hello_world2
+    puts 'hello world again'
+  end
+
+end
---
+++ TestGit2/HelloWorld.rb
@@ -0,0 +1,21 @@
+class HelloWorld
+  # To change this template use File | Settings | File Templates.
+  #Pre-requisites
+
+
+  #Instance Variables
+
+
+  #Class Variables
+
+
+  #Methods
+
+
+  def good_morning
+
+    puts 'good morning'
+
+  end
+
+end
